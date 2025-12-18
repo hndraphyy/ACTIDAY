5b20bc234c02242ea8709c58a60d240af4a73fe0
hndraphyy <hendraaditya1913@gmail.com>
2025-12-18 11:24:34 +0700

    chore: setup css

--- src/App.jsx
+++ src/App.jsx
@@ -1,80 +1,33 @@
 import { useState, useEffect } from "react";
 import { supabase } from "./supabaseClient";
 
-import "./App.css";
-
 function App() {
   const [todos, setTodos] = useState([]);
   const [selectedIds, setSelectedIds] = useState([]);
   const [taskName, setTaskname] = useState("");
 
-  const fetchTodos = async () => {
-    const { data, error } = await supabase
-      .from("todos")
-      .select("*")
-      .order("id", { ascending: false });
-
-    if (error) console.error(error);
-    else setTodos(data);
-  };
-
-  const handleSelect = (id) => {
-    if (selectedIds.includes(id)) {
-      setSelectedIds(selectedIds.filter((item) => item !== id));
-    } else {
-      setSelectedIds([...selectedIds, id]);
-    }
-  };
-
-  const deleteSelected = async () => {
-    if (selectedIds.length === 0) return;
-
-    const { error } = await supabase
-      .from("todos")
-      .delete()
-      .in("id", selectedIds);
-
-    if (error) {
-      console.error(error);
-    } else {
-      setSelectedIds([]);
-      fetchTodos();
-    }
-  };
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const addTodo = async (e) => {
-    e.preventDefault();
-    if (!taskName) return;
-
-    const { error } = await supabase.from("todos").insert([{ task: taskName }]);
-
-    if (error) {
-      console.error(error);
-    } else {
-      setTaskname("");
-      fetchTodos();
-    }
-  };
+  const today = new Date().toLocaleDateString("id-ID", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
 
   return (
     <div>
-      <h1>TODO LIST TODAY</h1>
-      <form onSubmit={addTodo}>
+      <h1 className="title">
+        ACTIDAY <span className="sub-title">ACTIVITY TODAY</span>
+      </h1>
+      <p>{today}</p>
+      <form>
         <div className="wp">
           <input
             type="text"
             placeholder="What Your Activity..."
-            value={taskName}
+            // value={taskName}
             onChange={(e) => setTaskname(e.target.value)}
           />
           <button type="submit">+</button>
-          <button onClick={deleteSelected} disabled={selectedIds.length === 0}>
-            x
-          </button>
+          <button disabled={selectedIds.length === 0}>x</button>
         </div>
       </form>
       <ul>
@@ -89,7 +42,6 @@ function App() {
           </li>
         ))}
       </ul>
-      {todos.length === 0 && <p>No Activity</p>}
     </div>
   );
 }
